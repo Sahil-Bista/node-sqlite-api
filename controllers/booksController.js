224ec1adf25cae9e95f6ff9d0beea979fc6eacea
sahilbista205@gmail.com <sahilbista205@gmail.com>
2025-09-11 17:07:26 +0545

    Fix : Fixed some typos in SQL to create books

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -4,14 +4,24 @@ import db from '../config/connDB.js';
 
 export const createBooks = asyncHandler(async(req, res)=>{
     const { title, isbn , published_year, author_id } = req.body;
-    const checkSQL = `
-        SELECT FROM books
+    const checksDuplicacySQL = `
+        SELECT * FROM books
         WHERE isbn = ?
     `;
-    const duplicate = await fetchFirst(db, checkSQL, [isbn] );
+    const duplicate = await fetchFirst(db, checksDuplicacySQL, [isbn] );
     if(duplicate){
         const error = new Error("Book with this isbn already exists");
         error.statusCode = 409; 
+        throw error;
+    }
+    const checkAuthorSQL = `
+        SELECT * FROM authors
+        WHERE id = ?
+    `
+    const author = await fetchFirst(db, checkAuthorSQL, [author_id]);
+    if(!author){
+        const error = new Error(`No such author with id ${author_id} exists in the author table`);
+        error.statusCode = 400; 
         throw error;
     }
     const sql = `INSERT INTO books
