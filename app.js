2f63bce9db1776b91181c24a0ec29b57524e79b8
sahilbista205@gmail.com <sahilbista205@gmail.com>
2025-09-14 12:56:53 +0545

    Feat : Added swagger configuration

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 import express from 'express';
 import router from './routes/api/index.js';
 import { globalErrorHandler } from './middlewares/globalErrorHandler.js';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerOptions from './swagger.json' assert {type : "json"};
 
 const app = express();
+
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
 
 app.use(express.json());
 
 app.use('/api', router);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use((req , res , next)=>{
     const error = new Error(`Route ${req.originalUrl} not found`);
